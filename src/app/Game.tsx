1fb1b7c82ee637f648e1818b94201da8ab10b514
Ankit singh <as1230641@gmail.com>
2024-09-06 16:18:34 +0530

    Add: "select" before difficulty

--- src/app/Game.tsx
+++ src/app/Game.tsx
@@ -71,7 +71,7 @@ const Game = () => {
             htmlFor="difficulty"
             className="mr-2 md:text-9xl font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient text-gray-500 font-serif text-xl"
           >
-            Difficulty:
+            Select Difficulty:
           </label>
           <select
             id="difficulty"
